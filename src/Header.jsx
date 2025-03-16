bd9d253a3cad3f5d06ee57c06339a2fd654a3ef3
Cem Burak Çevik <burak_cevik76@hotmail.com.tr>
2025-03-16 15:25:10 +0300

    Add Header.jsx

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './css/Header.css';
+
+function Header() {
+    return (
+        <div className='header'>
+            <h1 className='title'>My Courses</h1>
+        </div>
+    );
+}
+
+export default Header;
